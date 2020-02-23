4b178fb83e4e87cea46f92ed2955c417f3b248ee
manish mishra <manishmishra.dev@gmail.com>
2020-02-23 19:21:30 +0530

    guessing_game

---
+++ guessing_game.py
@@ -0,0 +1,16 @@
+import random
+
+random_number = random.randint(1, 10)
+yes = 'y'
+print("Hey welcome to the game....\nI have picked my number..Guess it?")
+
+while yes == 'y':
+    guess = int(input("your guess:\n"))
+    if guess == random_number:
+        print("You won")
+        yes = input("Want to play again: y/n\n")
+        break
+    elif guess < random_number:
+        print("too low")
+    else:
+        print("too high")
